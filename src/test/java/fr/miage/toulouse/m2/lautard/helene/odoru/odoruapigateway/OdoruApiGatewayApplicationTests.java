428fa9c606f78b0e238420cc893fcc54da9c6c39
HeleneLautard <helene.lautard@toulouse.miage.fr>
2022-06-23 17:20:39 +0200

    First commit

---
+++ src/test/java/fr/miage/toulouse/m2/lautard/helene/odoru/odoruapigateway/OdoruApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.miage.toulouse.m2.lautard.helene.odoru.odoruapigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OdoruApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
